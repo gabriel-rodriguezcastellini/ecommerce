95eb17486665d73a33d86e237c5b4f848db3f157
Gabriel Rodriguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2023-11-21 23:39:52 -0300

    NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,8 +9,19 @@ const NavBar = () => {
         <a className="navbar-brand" href="/">
           {brandName}
         </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <a className="nav-link" href="/">
                 Celulares

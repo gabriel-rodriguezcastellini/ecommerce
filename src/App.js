c3dda73ae2db0f9bb5b4ce3442623c843ba76593
Gabriel Rodriguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2023-11-26 20:28:35 -0300

    update app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 function App() {
+  useEffect(() => {
+    const tooltipTriggerList = [].slice.call(
+      document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
+    tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new window.bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
@@ -13,4 +25,3 @@ function App() {
 }
 
 export default App;
-//testing comment

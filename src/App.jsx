83f027a75f0e6cdcb8b22433896384589ce528a1
Gabriel Rodriguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2023-11-30 21:59:58 -0300

    add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import MainTitle from "./components/MainTitle/MainTitle";
+import NavBar from "./components/NavBar/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <NavBar />
+      <MainTitle />
+    </div>
+  );
 };
 
 export default App;

1200626874b1a99e3cba2f0318f6630333d144e5
Gabriel Rodriguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2023-12-13 09:35:33 -0300

    fix carousel bug when returning from a category page

--- src/components/CarouselItem/CarouselItem.jsx
+++ src/components/CarouselItem/CarouselItem.jsx
@@ -3,7 +3,7 @@ import { MDBCarouselItem } from "mdb-react-ui-kit";
 
 const CarouselItem = ({ item, index }) => {
   return (
-    <MDBCarouselItem itemId={index + 1}>
+    <MDBCarouselItem itemId={index + 1} className={index === 0 ? "active" : ""}>
       <img
         src={item.img}
         className="d-block w-25 h-25 mx-auto"

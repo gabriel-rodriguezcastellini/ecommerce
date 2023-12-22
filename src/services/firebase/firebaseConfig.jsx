f91ba533a7276ceade7a114f95c77efad6014b7b
Gabriel Rodriguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2023-12-21 21:58:58 -0300

    update Firebase configuration

--- src/services/firebase/firebaseConfig.jsx
+++ src/services/firebase/firebaseConfig.jsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, logEvent } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,4 +21,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+logEvent(analytics, "first_opened");
 export const db = getFirestore(app);
